9a50b7dd1b6c667f2e795fee5b1f453aaa48ea38
M1key-lamp <2026lampton.miking@fcboe.org>
2024-10-11 13:44:17 -0400

    Update fade.py

--- fade.py
+++ fade.py
@@ -9,38 +9,48 @@ i = 0
 red = (255,0,0)
 green = (0,255,0)
 blue = (0,0,255)
+yellow = (255,255,0)
+cyan = (0,255,255)
+magenta = (255,0,255)
+white = (255,255,255)
 
-def fade_out(color):
+clr_list = [red, green, white]
+
+
+def fade_out(color,speed):
 
     red_ratio = color[0]/50
     red_orig = color[0]
     green_ratio = color[1]/50
     green_orig = color[1]
     blue_ratio = color[2]/50
-    blue_orig = color[1]
+    blue_orig = color[2]
     for i in range(1,51):
-        red = int(red_orig - i * red_ratio)
+        r = int(red_orig - i * red_ratio)
         g = int(green_orig - i * green_ratio)
         b = int(blue_orig - i * blue_ratio)
-        np.fill((red,g,b))
+        np.fill((r,g,b))
+        print(np)
+        np[0]
         np.show()
-        time.sleep(.05)
+        time.sleep(speed)
 
-def fade_in(color):
-
+def fade_in(color,speed):
     red_ratio = color[0]/50
     red_orig = color[0]
     green_ratio = color[1]/50
     green_orig = color[1]
     blue_ratio = color[2]/50
-    blue_orig = color[1]
+    blue_orig = color[2]
     for i in range(1,51):
-        red = int(red_orig + i * red_ratio)
+        r = int(red_orig + i * red_ratio)
         g = int(green_orig +i * green_ratio)
         b = int(blue_orig + i * blue_ratio)
-        np.fill((red,g,b))
+        np.fill((r,g,b))
         np.show()
-        time.sleep(.05)
+        time.sleep(speed)
+
 while True:
-    fade_in((255,0,0))
-    fade_in((0,200,0))
+    rainbow_led = random.choice(clr_list)
+    fade_in(rainbow_led, 0.04)
+    fade_out(rainbow_led,.04)
